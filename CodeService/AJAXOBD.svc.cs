86d69da80e13c362883d71ba06d887d2bd0081ee
Jon McKinley <jmckinley@xenatech.com>
2017-06-01 08:31:33 -0600

    changes

--- CodeService/AJAXOBD.svc.cs
+++ CodeService/AJAXOBD.svc.cs
@@ -16,20 +16,7 @@ namespace CodeService
     [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class AJAXOBD
     {
-        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
-        // To create an operation that returns XML,
-        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
-        //     and include the following line in the operation body:
-        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
-        [OperationContract]
-        public void DoWork()
-        {
-            // Add your operation implementation here
-            return;
-        }
-
-        // Add more operations here and mark them with [OperationContract]
-
+        
         #region " Interfaces "
         [OperationContract]
         [WebGet]
